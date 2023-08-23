051ec4b1a4d146d513c60815af6dd40e59899ed0
chirag82124 <chiragsd94@gmail.com>
2023-08-23 18:37:52 +0530

    modified App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="flex justify-between mt-3 pb-2 pt-2 mb-5">
+  <div class="flex justify-between m-2 p-2">
   <div>
-  <router-link class="bg-yellow-500 m-2 p-2 rounded rounded-xl hover:bg-red-500" :to="{name:'home'}">Home</router-link>
-  <router-link class="bg-yellow-500 m-2 p-2 rounded rounded-xl hover:bg-red-500" :to="{name:'about'}">About</router-link>
+  <router-link :to="{name:'home'}">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+  <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+</svg>
+
+    </router-link>
+  <router-link :to="{name:'about'}">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 01-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 01-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 01-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584zM12 18a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
+</svg>  
+  </router-link>
 </div>
 <div>
-  <router-link class="text-orange-500 text-xl hover:bg-red-500 " :to="{name:'home'}">Meals</router-link>
+  <router-link class="text-pink-500 text-xl" :to="{name:'home'}">Meals</router-link>
 </div>
 <div>
-  <router-link class="bg-yellow-500 m-2 p-2 rounded rounded-xl hover:bg-red-500" :to="{name:'SearchMealByName'}">search</router-link>
+  <router-link  :to="{name:'SearchMealByName'}">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path d="M8.25 10.875a2.625 2.625 0 115.25 0 2.625 2.625 0 01-5.25 0z" />
+  <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.125 4.5a4.125 4.125 0 102.338 7.524l2.007 2.006a.75.75 0 101.06-1.06l-2.006-2.007a4.125 4.125 0 00-3.399-6.463z" clip-rule="evenodd" />
+</svg>
+
+  </router-link>
 </div>
 </div>
   <RouterView/>
 </template>
+<!-- 
+class="bg-yellow-500 m-1 p-1 rounded rounded-xl hover:bg-red-500" -->
